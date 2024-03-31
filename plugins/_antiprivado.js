55124d48cc6011efe3858bec70d3f0cf66f4d796
Daniel <159606732+Danielbot01@users.noreply.github.com>
2024-03-31 00:02:58 -0600

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -8,7 +8,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`_*< ANTI-PRIVADO />*_\n\n*Como te gusta mamar vrg pa al privado no se habla si quieres probar el bot unete https://chat.whatsapp.com/L0pLoI1DRRz58NV2mS6bop si quieres hablar con mi dueño solo manda mensaje a +52 729 9350963`, false, {mentions: [m.sender]});
+    await m.reply(`_*< ANTI-PRIVADO />*_\n\n*Como te gusta mamar vrg pa al privado no se habla si quieres probar el bot unete https://chat.whatsapp.com/Bdc1j3qNIU27mVhFsCwbFr si quieres hablar con mi dueño solo manda mensaje a +52 446 101 8899`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
